61d907481255c20395d93f777ffa0967d5aedf57
Kenneth Anguish <kenguish@gmail.com>
2011-12-28 00:42:15 +0800

    Initial Commit

---
+++ OSMOffline/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  OSMOffline
+//
+//  Created by Kenneth Lo on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
